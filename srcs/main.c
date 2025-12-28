bb64d22309845d2bae40c45ded9e7be87e35fa8d
ahmedaitelaaoud <ahmedaitelaaoud@gmail.com>
2025-12-28 12:33:33 +0100

    Refactor main function to remove redundant parse_args call and streamline sorting logic

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aait-ela <aait-ela@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 19:04:59 by aait-ela          #+#    #+#             */
-/*   Updated: 2025/12/27 23:27:12 by aait-ela         ###   ########.fr       */
+/*   Updated: 2025/12/28 12:26:47 by aait-ela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,31 +37,23 @@ int	main(int argc, char **argv)
 {
 	t_list	*stack_a;
 	t_list	*stack_b;
+	int		size;
 
 	stack_a = NULL;
 	stack_b = NULL;
 	parse_args(argc, argv, &stack_a);
-	parse_args(argc, argv, &stack_b);
-	// //TODO For swap
-	// sa(&stack_a);
-	// print_list(stack_a, stack_b);
-	// printf("\n");
-	// sb(&stack_b);
-	// print_list(stack_a, stack_b);
-	// printf("\n");
-	// ss(&stack_a, &stack_b);
-	// print_list(stack_a, stack_b);
-	// //TODO For push
-	// pa(&stack_b, &stack_a);
-	// print_list(stack_a, stack_b);
-	// printf("\n");
-	// pb(&stack_a, &stack_b);
-	// print_list(stack_a, stack_b);
-	// printf("\n");
-	// //TODO For rrotate
 	print_list(stack_a, stack_b);
 	printf("\n");
-	sort_five(&stack_a, &stack_b);
+	size = lst_size(stack_a);
+	if (size == 2)
+		sa(&stack_a);
+	else if (size == 3)
+		sort_three(&stack_a);
+	else if (size == 4)
+		sort_four(&stack_a, &stack_b);
+	else if (size == 5)
+		sort_five(&stack_a, &stack_b);
+	// else if (size > 5) sort_big(&stack_a, &stack_b);
 	print_list(stack_a, stack_b);
 	printf("\n");
 	free_stack(&stack_a);
